6e3a05b586792051c0950307f868e832387044da
Luksdanvip-OG <lucasbarberisperez@gmail.com>
2024-03-14 19:40:29 +0100

    Added picture on About.tsx

--- src/components/section/about/About.tsx
+++ src/components/section/about/About.tsx
@@ -7,9 +7,9 @@ function ProfileInfo() {
       <div className="bg-[url('./bg-about.svg')] p-4 rounded-md bg-cover bg-center w-full  flex flex-col justify-center items-center md:w-full bg-black">
         <div className="flex flex-col justify-center items-center w-full md:w-3/4 p-4  overflow-hidden scroll-smooth bg-black bg-opacity-45">
           <img
-            src="/me.jpg"
+            src="./pictureme.png"
             alt="photo"
-            className="w-32 h-32 md:w-52 md:h-52 rounded-full self-center"
+            className="w-32 h-32 md:w-48 md:h-48 rounded-full  object-scale-down  object-top bg-white"
           />
           <div className="w-full p-2 flex flex-col gap-4">
             <h2 className="text-3xl md:text-5xl font-bold text-center">
